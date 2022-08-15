cb96f68051b7b14989fd8b69ed336e918a519508
Gustavo Brandão Gimenez <70249205+gustavo-brandao@users.noreply.github.com>
2022-08-15 01:32:25 -0400

    While
    Como usar o comando de repetição while com PHP

---
+++ while_com_php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <title>Celke - While</title>
+</head>
+<body>
+
+    <?php
+
+    $a = 1;
+
+    //O while é o comando de repetição
+    while($a <= 10){
+        echo "Cadastro: $a <br>";
+
+        //Incrementar a condição utilizada no while
+        $a++;
+    }
+
+    ?>
+    
+</body>
+</html>
